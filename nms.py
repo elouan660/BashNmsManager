834c7a9641bab40b7a56652cc146d51ccfecb970
elouan660 <95432031+elouan660@users.noreply.github.com>
2022-04-09 21:56:54 +0200

    optimisation

--- nms.py
+++ nms.py
@@ -1,4 +1,5 @@
 import os as os
+
 #Définition des couleurs BASH
 class color:
     green = '\033[92m'
@@ -6,26 +7,35 @@ class color:
     red = '\033[91m'
     reset = '\033[0m'
     blue = '\033[0;34m'
+
 program = f"{color.yellow}Program: {color.reset}"
 def positive():
-    print(f"{program}{color.green}[mods activés]")
-    print(f"{program}liste des mods:")
+    print(f"{program}{color.green}[mods activés]{color.reset}")
+    print(f"{color.yellow}liste des mods:")
     files = os.listdir("/home/elouandeschamps/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/No Man's Sky/GAMEDATA/PCBANKS/MODS")
     for name in files:
         print(f"{color.blue}-{color.reset}{name}")
-try:
-    #Activation des mods
-    test = os.path.join("""/home/elouandeschamps/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/No Man's Sky/GAMEDATA/PCBANKS/""", ".DISABLEMODS.TXT")
-    test2 = os.path.join("""/home/elouandeschamps/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/No Man's Sky/GAMEDATA/PCBANKS""", "DISABLEMODS.TXT")
-    os.rename(test, test2)
-    positive()
-except FileNotFoundError:
-    #Désactivation des mods
-    request1 = input(f"{program}Désactiver les mods? (y/n) ")
-    if request1 == "y":
+
+
+files0 = os.listdir("/home/elouandeschamps/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/No Man's Sky/GAMEDATA/PCBANKS")
+
+if ".DISABLEMODS.TXT" in files0:
+    request = input(f"{program}Mods désactivés, souhaitez vous les activer? (y/n) ")
+    if request == "y":
+        test = os.path.join("""/home/elouandeschamps/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/No Man's Sky/GAMEDATA/PCBANKS/""", ".DISABLEMODS.TXT")
+        test2 = os.path.join("""/home/elouandeschamps/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/No Man's Sky/GAMEDATA/PCBANKS""", "DISABLEMODS.TXT")
+        os.rename(test, test2)
+        positive()
+    else:
+        print(f"{program}{color.red}[mods désactivés]")
+elif "DISABLEMODS.TXT" in files0:
+    request = input(f"{program}Mods activés, souhaitez vous les desactiver? (y/n) ")
+    if request == "y":
         test = os.path.join("""/home/elouandeschamps/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/No Man's Sky/GAMEDATA/PCBANKS/""", ".DISABLEMODS.TXT")
         test2 = os.path.join("""/home/elouandeschamps/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/No Man's Sky/GAMEDATA/PCBANKS""", "DISABLEMODS.TXT")
         os.rename(test2, test)
         print(f"{program}{color.red}[mods désactivés]")
     else:
-        positive()
+        print(f"{program}{color.red}[mods désactivés]")
+else:
+    print(f"{program}disablemods non trouvé")
